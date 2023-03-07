2806288b58ae98259e42bf917d58cf28e894a580
glen shi <102923265+GlebwithB@users.noreply.github.com>
2023-03-07 15:24:57 -0500

    Delete laurier_base.py

--- laurier_base.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd, selenium, requests, numpy as np, matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
-from functions import safe_print, safe_transfer, type_print
-
-url = "https://www.sunshineliststats.com/Employer/9/2021/?n=wilfridlaurieruniversity"
-html = safe_transfer(requests.get(url).text)
-
