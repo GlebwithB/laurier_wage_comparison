92fcd7554b3effb787643ea5f4aba1205de6ee3a
glen shi <102923265+GlebwithB@users.noreply.github.com>
2023-03-08 00:47:43 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,9 +1,5 @@
 from bs4 import BeautifulSoup
-import numpy as np
-import requests
-import selenium
-import sys
-
+import numpy as np, requests, selenium, sys, matplotlib.pyplot as plt 
 
 def safe_print(data):
     # can also use 'replace' instead of 'ignore' for errors= parameter
@@ -19,10 +15,13 @@ def type_print(data):
 
     safe_print(type(data), "\n", str(data), "\n", len(data))
 
-def cleanse(list_set, keywords = {"":1}):
-        for key in keywords.keys:
-            for item in (list_set, keywords[key]):
-                item.replace(key, "")
+def cleanse(list_set, keywords = {" ":1}):
+        
+        for x in list(keywords.keys()):
+            print(x)
+            list_set = [list_set[y].replace(x, "")for y in range(0, len(list_set), keywords[x])]
+    
+        return list_set
 
 
 
@@ -32,15 +31,11 @@ def scrap_web(url, parser, content_parse = {"Name": False, "Year": False, "Salar
     def safe_transfer(data):
         data = (str(data).encode(sys.stdout.encoding, errors='replace').decode(sys.stdout.encoding))
         return data
-
-    
-
-
     #temporary to test true or false
     #content_parse = {"Name": True, "Year": True, "Salary":True, "Position": True, "Employer":True}
 
     soup = BeautifulSoup(safe_transfer(requests.get(url).text), parser)
-    data = [(x.text.strip().replace("\n", "").replace("\r", "")) for x in soup.find("tbody").find_all("td")]
+    data = [(x.text.strip().replace("\n", "").replace("\r", "")).replace(",", "") for x in soup.find("tbody").find_all("td")]
     
 
     return data
